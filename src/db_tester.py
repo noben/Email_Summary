b1c4a46c87b42be2193a7dfd4634ba617cf34e15
Kevin <zhao.yuan.work@gmail.com>
2013-11-26 17:34:32 -0500

    (1)speech act tag finished (2)import bc3 corpus into sqllite finished

---
+++ src/db_tester.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+conn = sqlite3.connect('../bc3/bc3.db')
+
+c = conn.cursor()
+
+print "========================================Thread============================"
+for row in c.execute('SELECT * FROM thread'):
+        print row
+print "========================================email============================"        
+for row in c.execute('SELECT * FROM email'):
+        print row
+print "========================================sentence============================"        
+for row in c.execute('SELECT * FROM sentence'):
+        print row
--- src/main.py
+++ src/main.py
@@ -1,55 +1,119 @@
 import xml.etree.ElementTree as ET
 import sqlite3
 import os
+import subprocess
 
+# Read XML files and parse it into memory
+bc3_corpus_xml_doc = ET.parse('../bc3/corpus.xml')
+bc3_annotation_xml_doc = ET.parse('../bc3/annotation.xml')
+    
+'''
+TODO:give a similarity score for each sentence by comparing it with the email subject 
+@author: Kevin Zhao
+'''   
 def get_subject_similarity(text):
     return 10
-
-#delete DB file in testing
-os.remove("../bc3/bc3.db")
-
-conn = sqlite3.connect('../bc3/bc3.db')
-c = conn.cursor()
-# Create table
-c.execute('''CREATE TABLE thread
-             (id,subject)''')
-c.execute('''CREATE TABLE email
-             (id,thread_id,subject,
-             from_who,
-             to_whom,
-             cc,sa_tag)''')
-c.execute('''CREATE TABLE sentence
-             (id,email_id,text,length,similarity)''')
-
-
-tree = ET.parse('../bc3/corpus.xml')
-root = tree.getroot()
-#iterate through all the "thread" tags
-thread_no = 0
-for thread in root:
-    subject = thread[0].text
-    # Insert a row of data in thread table
-    c.execute("INSERT INTO thread VALUES (?,?)", (thread_no, subject))
-    #iterate through all the "DOC" tags under the "thread" tags
-    email_no = 0
-    for email in thread.findall('.//DOC'):
-        from_who = email[1].text
-        to_whom = email[2].text
-        email_subject = email[3].text
-        #Insert a row of data in email table
-        c.execute("INSERT INTO email VALUES (?,?,?,?,?,?,?)", (email_no, thread_no, email_subject, from_who, to_whom,"", ""))
-        
-        sentence_no = 0
-        for sentence in email.findall('.//Text/Sent'):
-            #Insert a row of data in sentence table
-            c.execute("INSERT INTO sentence VALUES (?,?,?,?,?)", (sentence_no, email_no, sentence.text, len(sentence.text), get_subject_similarity(sentence.text)))
-            sentence_no = sentence_no + 1
-            
-        email_no = email_no + 1
-        
-    thread_no = thread_no + 1
+'''
+Look bc3 corpus xml file into sqllite
+@author: Kevin Zhao
+'''
+def load_bc3_corpus():
+    #TODO:delete DB file in test setting
+    os.remove("../bc3/bc3.db")
+    # Load database file
+    conn = sqlite3.connect('../bc3/bc3.db')
+    # Get database cursor
+    db_cursor = conn.cursor()
+    # Create table
+    db_cursor.execute('''CREATE TABLE thread
+                 (id,subject)''')
+    db_cursor.execute('''CREATE TABLE email
+                 (id,thread_id,subject,
+                 from_who,
+                 to_whom,
+                 cc)''')
+    db_cursor.execute('''CREATE TABLE sentence
+                 (id,email_id,text,length,similarity,extracted,sa_tag)''')
+#    db_cursor.execute('''CREATE TABLE sentence_summary
+#                 (id,text,)''')
     
-# Save (commit) the changes
-conn.commit()
+    
+    # Get the root node in the xml file
+    xml_root_node = bc3_corpus_xml_doc.getroot()
+    #iterate through all the "thread_node" tags
+    for thread_node in xml_root_node:
+        subject = thread_node[0].text
+        thread_list_no = thread_node[1].text
+        # Insert a row of data in thread_node table
+        db_cursor.execute("INSERT INTO thread VALUES (?,?)", (thread_list_no, subject))
+        #iterate through all the "DOC" tags under the "thread_node" tags
+        email_no = 0
+        for email_node in thread_node.findall('.//DOC'):
+            from_who = email_node[1].text
+            to_whom = email_node[2].text
+            email_subject = email_node[3].text
+            #Insert a row of data in email_node table
+            db_cursor.execute("INSERT INTO email VALUES (?,?,?,?,?,?)", (email_no, thread_list_no, email_subject, from_who, to_whom, ""))
+            
+            sentence_no = 0
+            for sentence_node in email_node.findall('.//Text/Sent'):
+                #Insert a row of data in sentence_node table
+                db_cursor.execute("INSERT INTO sentence VALUES (?,?,?,?,?,?,?)", (sentence_no, email_no, sentence_node.text, len(sentence_node.text), get_subject_similarity(sentence_node.text), is_sentence_extracted(thread_list_no, email_no, sentence_no), ""))
+                sentence_no = sentence_no + 1
+                
+            email_no = email_no + 1
+            
+        
+    # Save (commit) the changes
+    conn.commit()
+    
+    conn.close()
+    
+    
+def get_thread_node_by_list_no(thread_list_no):
+    # Get the root node in the xml file
+    xml_root_node = bc3_annotation_xml_doc.getroot()
+    # Get the unique thread_node through thread_list_no
+    thread_node = bc3_annotation_xml_doc.findall(".//thread/[listno='" + thread_list_no + "']")
+    
+    return thread_node[0]
 
-conn.close()
+'''
+if the sentence has been selected as one of the extractions by looking at annotation.xml
+@author: Kevin Zhao
+'''
+def is_sentence_extracted(thread_list_no, email_id, sentence_id):
+    thread_node = get_thread_node_by_list_no(thread_list_no)
+    # construct sentence_id in the form of 1.2,1.3,1.4.....
+    sentence_unque_id = str(email_id) + "." + str(sentence_id)
+    # Find all the sent_node under the thread_node whose id is sentence_unque_id
+    result_hits = thread_node.findall(".//annotation/sentences/sent[@id='" + sentence_unque_id + "']")
+    
+    return len(result_hits) != 0
+#    
+#def load_bc3_summary(thread_list_no):
+#    # Load database file
+#    conn = sqlite3.connect('../bc3/bc3.db')
+#    # Get database cursor
+#    db_cursor = conn.cursor()
+#    
+#    thread_node = get_thread_node_by_list_no(thread_list_no)
+#    # Find all the sent_node under the thread_node whose id is sentence_unque_id
+#    for sent_node in thread_node.findall('.//annotation/summary/sent'):
+#        # Insert a row of data in summary table
+#        db_cursor.execute("INSERT INTO summary VALUES (?,?)", (thread_no, subject))
+
+'''
+Run Speech act on email text and update sa_tag column in sentence table
+@author: Kevin Zhao
+'''
+def load_generated_speech_act_tag():
+    subprocess.call(['java', '-jar', '../libs/speech_act.jar', '../bc3/bc3.db'])
+    
+def main():
+    load_bc3_corpus()
+##    load_bc3_summary()
+    load_generated_speech_act_tag()
+    
+if __name__ == "__main__":
+    main()
