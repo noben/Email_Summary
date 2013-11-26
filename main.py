5eabbe0afab0e83b5a0ba5522a1717301c27fad2
Kevin <zhao.yuan.work@gmail.com>
2013-11-25 19:36:57 -0500

    xml parser initial steps

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import xml.etree.ElementTree as ET
 tree = ET.parse('corpus.xml')
 root = tree.getroot()
+#iterate through all the "thread" tags
 for thread in root:
-    thread
+    subject = thread[0].text
+    #iterate through all the "DOC" tags under the "thread" tags
+    for email in thread.findall('.//DOC'):
+        from_who = email[1]
+        to_whom = email[2]
+        email_subject = email[3]
